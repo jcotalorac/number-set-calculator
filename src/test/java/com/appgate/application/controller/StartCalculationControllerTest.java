d51773e1be127fcf698f415694c3a728c617c7e1
Juan Carlos Otálora <jcotalorac@gmail.com>
2021-10-21 11:56:45 -0500

    Start calculation controller unit test

---
+++ src/test/java/com/appgate/application/controller/StartCalculationControllerTest.java
@@ -0,0 +1,32 @@
+package com.appgate.application.controller;
+
+import com.appgate.application.usecase.StartCalculation;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+class StartCalculationControllerTest {
+
+    @InjectMocks
+    private StartCalculationController startCalculationController;
+
+    @Mock
+    private StartCalculation startCalculation;
+
+    @Test
+    void testSuccessfulStartCalculation() {
+        when(startCalculation.apply()).thenReturn(UUID.randomUUID());
+
+        UUID uuid = startCalculationController.startCalculation();
+
+        assertNotNull(uuid);
+    }
+}
