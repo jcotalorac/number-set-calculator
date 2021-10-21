f16018b59511754667d084ec819acabae5e20f17
Juan Carlos Otálora <jcotalorac@gmail.com>
2021-10-21 17:00:10 -0500

    Basic logging

--- src/main/java/com/appgate/application/usecase/impl/StartCalculationImpl.java
+++ src/main/java/com/appgate/application/usecase/impl/StartCalculationImpl.java
@@ -1,15 +1,20 @@
 package com.appgate.application.usecase.impl;
 
 import com.appgate.application.usecase.StartCalculation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class StartCalculationImpl implements StartCalculation {
 
     @Override
     public UUID apply() {
-        return UUID.randomUUID();
+        log.info("Start calculation use case");
+        UUID uuid = UUID.randomUUID();
+        log.debug("Returning operation id: " + uuid);
+        return uuid;
     }
 }
--- src/test/java/com/appgate/application/usecase/StartCalculationTest.java
+++ src/test/java/com/appgate/application/usecase/StartCalculationTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ActiveProfiles("test")
 @ExtendWith(SpringExtension.class)
