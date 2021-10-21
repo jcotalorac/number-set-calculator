12da1b3a07ce36c89f9241cb497a44b9f0e8dae6
Juan Carlos Otálora <jcotalorac@gmail.com>
2021-10-21 13:51:14 -0500

    Endpoint documentation

--- src/main/java/com/appgate/application/controller/StartCalculationController.java
+++ src/main/java/com/appgate/application/controller/StartCalculationController.java
@@ -2,6 +2,7 @@ package com.appgate.application.controller;
 
 import com.appgate.application.controller.dto.StartCalculationResponse;
 import com.appgate.application.usecase.StartCalculation;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class StartCalculationController {
     @Autowired
     private StartCalculation startCalculation;
 
+    @Operation(summary = "Initialize operation returning an id")
     @GetMapping("/startCalculation")
     public StartCalculationResponse startCalculation() {
         return StartCalculationResponse.builder()
